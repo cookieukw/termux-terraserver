b3ae926f7cf5b4f910ef047026c6b096b7965793
CookieUkw <65344982+cookieukw@users.noreply.github.com>
2023-07-26 12:40:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,13 +17,11 @@ curl -O https://terraria.org/api/download/pc-dedicated-server/terraria-server-14
 unzip terraria-server-1449.zip
 rm terraria-server-1449.zip
 chmod +x 1449/Linux/TerrariaS*
-echo -e "mono --server -O=all ./TerrariaServer.exe" > 1449/Linux/run.sh
-chmod +x 1449/Linux/run.sh
 rm 1449/Linux/System*
 rm 1449/Linux/Mono*
 rm 1449/Linux/monoconfig
 rm 1449/Linux/mscorlib.dll
+echo -e "mono --server -O=all ./TerrariaServer.exe" > 1449/Linux/run.sh
+chmod +x 1449/Linux/run.sh
+mv 1449 ..
 cls
-echo -e "Select Grimle repo in main repository"
-sleep 5
-termux-change-repo
