e05e5c9db5fc51f770357f13a63b3d2e5fbad26c
CookieUkw <65344982+cookieukw@users.noreply.github.com>
2023-07-26 11:46:03 -0300

    Update and rename init.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt upgrade -y && apt update
+pkg install unzip
+curl -O https://terraria.org/api/download/pc-dedicated-server/terraria-server-1449.zip
+unzip terraria-server-1449.zip
+rm terraria-server-1449.zip
+chmod +x 1449/Linux/TerrariaS*
+rm 1449/Linux/System*
+rm 1449/Linux/Mono*
+rm 1449/Linux/monoconfig
+rm 1449/Linux/mscorlib.dll
+cls
+echo -e "Select Grimle repo in main repository"
+sleep 5
+termux-change-repo
