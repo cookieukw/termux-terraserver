ec5bab4ba7a3e680dbebc7cadc8c0ed1e017b61f
CookieUkw <65344982+cookieukw@users.noreply.github.com>
2023-07-26 12:11:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ curl -O https://terraria.org/api/download/pc-dedicated-server/terraria-server-14
 unzip terraria-server-1449.zip
 rm terraria-server-1449.zip
 chmod +x 1449/Linux/TerrariaS*
+echo -e "mono --server -O=all ./TerrariaServer.exe" > 1449/Linux/run.sh
+chmod +x 1449/Linux/run.sh
 rm 1449/Linux/System*
 rm 1449/Linux/Mono*
 rm 1449/Linux/monoconfig
